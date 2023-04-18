fa5afca6b5f71300b10d7d82f362ab11eab5fd76
qa-pdhvq-32-mvwt@example.com <qa-pdhvq-32-mvwt@example.com>
2023-04-18 18:04:35 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16002.stoplight-dev.com/api/v1/projects/cHJqOjEwNzA/spectral.js?branch_slug=main&token=ff861e83-1221-46e2-a349-56fa110a35d9";
+export default { extends: ruleset };
